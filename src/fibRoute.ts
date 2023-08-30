80727a21fb9e60f04442b4ec3631e2067862eb7d
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-08-30 16:02:50 -0400

    fixed

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -7,7 +7,7 @@ export default (req: Request, res: Response) => {
   const num: string = req.params.num;
 
   const fibN: number = fibonacci(parseInt(num));
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
